9d0ec6d62d55ceb73f4c4f52f08bb532256b1eff
coderman <gyb47434@gmail.com>
2025-12-06 10:59:44 +0800

    shadcn button test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <RouterView />
+  <div>
+    <Button variant="destructive">Click me</Button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import { Button } from '@/components/ui/button'
 </script>
